b0fb5867fe4ffcbb304772a9389bc3ad413ac7a0
Ouael Naouri <62753581+ouaeln@users.noreply.github.com>
2021-10-28 04:44:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def get(addresse = 'searchedaddress'):
     #GET SUGGESTED ADDRESSES
     responseforaddressapi = requests.get('https://api-adresse.data.gouv.fr/search/', params=dataforaddressapi)
     dictionary = responseforaddressapi.json()
+    adresse1 = dictionary.get('features')[0].get('properties').get('label')
 
     #CHOOSE FIRST SUGGESTION
     coordinatesarray = dictionary.get('features')[0].get('geometry').get('coordinates')
@@ -59,14 +60,20 @@ def get(addresse = 'searchedaddress'):
 
     #GETTING RESPONSE FROM SERVER
     returneddata= requests.post('http://64.225.65.140:8080/geoserver/wfs?outputFormat=application/json', data=xml).json()
-
     if len(returneddata.get('features')) > 0:
-        return returneddata.get('features')[0].get('properties').get('ALEA')
+        ALEA = returneddata.get('features')[0].get('properties').get('ALEA')
+        response = {
+            'Addresse': adresse1,
+            'NiveauAlea': "Exposition au retrait-gonflement des sols argileux : Alea " + ALEA
+        }
+        return response
     else:
-        return "Non"
+        response = {
+            'Addresse': adresse1,
+            'NiveauAlea': "Exposition au retrait-gonflement des sols argileux : Non"
+        }
+        return response
 
-
-
-    if name == "main":
-        app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
