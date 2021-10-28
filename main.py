c8499f61abc549cf3167152b07371225fa4377a6
Ouael Naouri <62753581+ouaeln@users.noreply.github.com>
2021-10-28 05:56:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,12 +63,8 @@ def get(addresse = 'searchedaddress'):
     returneddata= requests.post('http://64.225.65.140:8080/geoserver/wfs?outputFormat=application/json', data=xml).json()
     if len(returneddata.get('features')) > 0:
         ALEA = returneddata.get('features')[0].get('properties').get('ALEA')
-        return jsonify({'Addresse':adresse1, 'NiveauAlea': "Exposition au retrait-gonflement des sols argileux : Alea " + ALEA}), 200
+        return jsonify({'Addresse':adresse1, 'NiveauAlea': "Exposition au retrait-gonflement des sols argileux : Aléa " + ALEA}), 200
     else:
-        response = {
-            'Addresse': adresse1,
-            'NiveauAlea': "Exposition au retrait-gonflement des sols argileux : Non"
-        }
         return jsonify({'Addresse':adresse1, 'NiveauAlea': "Exposition au retrait-gonflement des sols argileux : Non"}), 200
 
 if __name__ == "__main__":
